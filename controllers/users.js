eeeab98e30aec76ef5ac10e7022797a77d6b2edb
Алексей Фёдоров <67751797+RLYamakasi@users.noreply.github.com>
2022-10-20 00:09:49 +0300

    first commit testing+

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const users = require('../models/users');
-const express = require(`express`);
-
+const express = require('express');
 const app = express()
 module.exports = router;
 
@@ -15,26 +14,27 @@ module.exports.postUser = (req, res) => {
   const {name,avatar,about} = req.body;
   const user = new users({name,avatar,about});
   return user
-    .save()
-    .then((users) => res.send({ users }))
-    .catch((err) => console.log(err))
+  .save()
+  .then((users) => res.send({ users }))
+  .catch((err) => res.send({ message: err.message }));
 };
 
 module.exports.getUserById = (req, res) => {
   users.findById(req.params.id)
-    .then((users) => res.send({ users }))
-    .catch((err) => res.send({ message: err.message }));
+  .then((users) => res.send({ users }))
+  .catch((err) => res.send({ message: err.message }));
 };
 
+
 module.exports.patchUserInfo = (req, res) => {
-  const { name, about } = req.body;
-  users.findByIdAndUpdate(
-    req.user._id,
-    { name, about },
-    { new: true },
-  )
-    .then((user) => res.send(user))
-    .catch((err) => res.send({ message: err.message }));
+    const { name, about } = req.body;
+    users.findByIdAndUpdate(
+      req.user._id,
+      { name, about },
+      { new: true },
+    )
+      .then((user) => res.send(user))
+      .catch((err) => res.send({ message: err.message }));
 };
 
 module.exports.patchUserAvatar = (req, res) => {
